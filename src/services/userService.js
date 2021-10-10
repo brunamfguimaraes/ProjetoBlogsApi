a837d8d2f041b40a0605b0d92f67ecad3e148ed2
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-10 16:11:31 -0300

    set userService

---
+++ src/services/userService.js
@@ -0,0 +1,52 @@
+const { User } = require('../models');
+
+function validateDisplayName(displayName) {
+  if (displayName.length < 8) return false;
+
+  return true;
+}
+
+function validateEmail(email) {
+  if (!email) {
+    return null;
+  }
+  
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!email.match(emailRegex)) {
+    return false;
+  }
+
+  return true;
+}
+
+function validatePassword(password) {
+  if (!password) {
+    return null;
+  }
+
+  if (password.length < 6) {
+    return false;
+  }
+
+  return true;
+}
+
+async function create(body) {
+  const { email } = body;
+
+  const userExists = await User.findOne({ where: { email } });
+  if (userExists) {
+    return false;
+  }
+  
+  const newUser = await User.create(body);
+
+  return newUser;
+}
+
+module.exports = {
+  validateDisplayName,
+  validateEmail,
+  validatePassword,
+  create,
+};
