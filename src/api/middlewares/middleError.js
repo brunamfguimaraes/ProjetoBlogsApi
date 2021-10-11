8a0a40a04c99f3de2d87b480b8d60288e9d67478
joao966 <joaooliveira966@gmail.com>
2021-10-11 02:43:10 -0300

    FIX

--- src/api/middlewares/middleError.js
+++ src/api/middlewares/middleError.js
@@ -7,7 +7,9 @@ const errorMiddleware = (err, _req, res, _next) => {
     return res.status(err.code)
     .json({ message: err.message });
   }
-  return res.status(200)
+  res.status(200)
+    .json({ error: { code: 'internal', message: 'Internal server error' } });
+    res.status(201)
     .json({ error: { code: 'internal', message: 'Internal server error' } });
 };
 
