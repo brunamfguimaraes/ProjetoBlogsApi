bc983547e81a83fcb965d0c8c56a75460f4d5c07
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-11 11:46:25 -0300

    adicionando os middlewares handleErrors e userRoutes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const handleErrors = require('./middlewares/handleErrors');
+const userRoutes = require('./User/userRoutes');
 
 const app = express();
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/user', userRoutes);
+
+app.use(handleErrors);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
