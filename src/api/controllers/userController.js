aedd8764061c0306fa15561c9488ac567fd723d1
joao966 <joaooliveira966@gmail.com>
2021-10-11 02:01:31 -0300

    FIX

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,11 +1,11 @@
-const rescue = require('express-rescue');
+// const rescue = require('express-rescue');
 require('dotenv').config();
 const jtw = require('jsonwebtoken');
 const { createUser } = require('../services/userService');
 
 console.log('controller');
 
-const post = rescue(async (req, res) => {
+const post = async (req, res) => {
   console.log('post');
   console.log(req.body);
   const { password, ...payload } = req.body;
@@ -18,7 +18,7 @@ const post = rescue(async (req, res) => {
   const token = jtw.sign(payload, process.env.SECRET, { expiresIn: '45m' });
   console.log('response');
   return res.status(201).json({ token });
-});
+};
 
 module.exports = {
   post,
