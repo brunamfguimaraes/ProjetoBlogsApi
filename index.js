e554a0f85a580a9bb4d3cbd4d47a829fd9faea62
Leandro Reis <lleandrofr@gmail.com>
2021-10-11 17:55:42 -0300

    Initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
-// não remova esse endpoint, e para o avaliador funcionar
+// do not remove this endpoint, it is for the evaluator to work correctly
 app.get('/', (request, response) => {
   response.send();
 });
