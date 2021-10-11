188f6ca525959e3fdc50b54c5c32b9fff450d9e8
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-11 15:16:12 -0300

    adicionando o endpoint GET "/user"

--- User/userRoutes.js
+++ User/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const validateJWT = require('../auth/validateJWT');
 const controller = require('./userController');
 
 const router = express.Router();
 
-router.route('/').post(controller.create);
+router.route('/')
+  .post(controller.create)
+  .get(validateJWT, controller.getAll);
 
 module.exports = router;
