cc5b5ac81a30a006df8e9f2dfa8ccb8ce1730156
esionascimento <esio.392@protonmail.com>
2021-10-11 03:39:34 -0400

    mudanca id -> postId migrations

--- src/migrations/20211011051237-create-posts-categories.js
+++ src/migrations/20211011051237-create-posts-categories.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('PostsCategories', {
-      id: {
+      postId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
