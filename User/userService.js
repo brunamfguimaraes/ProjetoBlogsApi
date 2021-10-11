17f1c7b1237fdf1d7be8d350baeb67ac2f9360b9
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-11 14:26:09 -0300

    alterando a validação da função requiredValidation para undefined

--- User/userService.js
+++ User/userService.js
@@ -5,7 +5,7 @@ const RequestError = require('../helper/customErrors');
 
 const requiredValidation = (fields = {}) => {
   const fieldsKeys = Object.keys(fields);
-  const invalidField = fieldsKeys.find((fieldKey) => !fields[fieldKey]);
+  const invalidField = fieldsKeys.find((fieldKey) => fields[fieldKey] === undefined);
   if (invalidField) throw new RequestError('badRequest', `"${invalidField}" is required`);
 };
 
