290e5c6e99dce4ca5677adb18ed7e14f6b860075
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-11 16:32:29 -0300

    adicionando o endpoint POST "/categories"

---
+++ Categories/categoriesRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const validateJWT = require('../auth/validateJWT');
+const controller = require('./categoriesController');
+
+const router = express.Router();
+
+router.route('/').post(validateJWT, controller.create);
+
+module.exports = router;
