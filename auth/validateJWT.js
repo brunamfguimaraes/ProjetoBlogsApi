35c27a9687c046f1c647f95fdbd309d75de4c179
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-11 15:14:56 -0300

    adicionando middleware de validação de token

---
+++ auth/validateJWT.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const rescue = require('express-rescue');
+const RequestError = require('../helper/customErrors');
+
+require('dotenv').config();
+
+module.exports = rescue(async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    throw new RequestError('unauthorized', 'Token not found');
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, _decoded) => {
+    if (err) throw new RequestError('unauthorized', 'Expired or invalid token');
+  });
+  next();
+});
