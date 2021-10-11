eaba6d8eea3895d7fcbe31fbdd44c48796ecc3d5
joao966 <joaooliveira966@gmail.com>
2021-10-10 22:20:44 -0300

    FIX: REQ-1

--- src/api/middlewares/middleError.js
+++ src/api/middlewares/middleError.js
@@ -1,8 +1,6 @@
 const errorMiddleware = (err, _req, res, _next) => {
-  if (err.isError) {
-    return res.status(err.code).json({ message: err.message });
-  }
   console.log(err);
+  return res.status(err.code).json({ message: err.message });
 };
 
 module.exports = errorMiddleware;
