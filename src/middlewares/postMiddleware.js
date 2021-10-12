72885b8ad13c85edc4b3008d60af47a6e542aaa9
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-12 17:32:08 -0300

    create middlewares post

---
+++ src/middlewares/postMiddleware.js
@@ -0,0 +1,63 @@
+const postService = require('../services/postService');
+
+function validateTitle(req, res, next) {
+  try {
+    const { title } = req.body;
+
+    const newPost = postService.validateTitle(title);
+    if (newPost === null) {
+      return res.status(400)
+        .json({ message: '"title" is required' });
+    }
+    
+    next();
+  } catch (error) {
+    console.log(`An unknown error has occurred: ${error}`);
+    return res.status(500).json({ message: 'An unknown error has occurred' });
+  }
+}
+
+function validateContent(req, res, next) {
+  try {
+    const { content } = req.body;
+
+    const newPost = postService.validateContent(content);
+    if (newPost === null) {
+      return res.status(400)
+        .json({ message: '"content" is required' });
+    }
+    
+    next();
+  } catch (error) {
+    console.log(`An unknown error has occurred: ${error}`);
+    return res.status(500).json({ message: 'An unknown error has occurred' });
+  }
+}
+
+async function validateContegoryIds(req, res, next) {
+  try {
+    const { categoryIds } = req.body;
+
+    const newPost = await postService.validateContegoryIds(categoryIds);
+    if (newPost === null) {
+      return res.status(400)
+        .json({ message: '"categoryIds" is required' });
+    }
+    
+    if (!newPost) {
+      return res.status(400)
+        .json({ message: '"categoryIds" not found' });
+    }
+    
+    next();
+  } catch (error) {
+    console.log(`An unknown error has occurred: ${error}`);
+    return res.status(500).json({ message: 'An unknown error has occurred' });
+  }
+}
+
+module.exports = {
+  validateTitle,
+  validateContent,
+  validateContegoryIds,
+};
