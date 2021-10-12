9eb2b84bb86c0bf26c9561d86881c8b07f5154d1
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-12 02:32:53 -0300

    Create express user controller

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { postUserService } = require('../services');
+
+const postUserController = async (req, res, next) => { 
+  const { displayName, email, password, image } = req.body;
+  
+  const errorOnPostUser = await postUserService(displayName, email, password, image);
+  if (errorOnPostUser) {
+    return next(errorOnPostUser);
+  }
+
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+  const token = jwt.sign({ /* data: user */ }, process.env.JWT_SECRET, jwtConfig);
+
+  res.status(201).json({ token });
+};
+
+module.exports = { postUserController };
