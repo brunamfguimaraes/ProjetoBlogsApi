f45f9c971cb14db0546944732a2e37f2dff15f46
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-12 02:25:28 -0300

    applying userRouter on /user route

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const middlewares = require('./middlewares');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/user', routers.userRouter);
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
