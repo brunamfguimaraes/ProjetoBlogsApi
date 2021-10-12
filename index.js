32f50760ff8a6846986b4a577359dbe61a113499
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-12 02:24:03 -0300

    importing routers and middlewares

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routers = require('./routers');
+const middlewares = require('./middlewares');
 
 const app = express();
 
