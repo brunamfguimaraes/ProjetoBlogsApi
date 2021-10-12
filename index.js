696c934deab1df0a899ce5ebf8e796e62d04352f
esionascimento <esio.392@protonmail.com>
2021-10-12 14:24:02 -0400

    mudanca index.js / new filer app

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const error = require('./src/api/middleware/error/error');
 require('dotenv').config();
+const app = require('./src/api/app');
 
-const app = express();
-app.use(bodyParser.json());
-
-const routerUser = require('./src/api/route/routeUser');
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || '3000';
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
-
-app.use('/user', routerUser);
-
-app.use(error);
---
+++ src/api/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const error = require('./middleware/error/error');
+require('dotenv').config();
+
+const routerUser = require('./route/routeUser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (request, response) => {
+  response.send();
+});
+
+app.use('/user', routerUser);
+
+app.use(error);
+
+module.exports = app;
