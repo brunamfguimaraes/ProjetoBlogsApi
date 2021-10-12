c10f28d726a50b7886e2a2f18213614bf7ef408f
joao966 <joaooliveira966@gmail.com>
2021-10-12 14:39:05 -0300

    FIX

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const app = require('./app');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`app rodando na porta ${PORT}`));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
-
-app.listen(PORT, () => console.log(`app rodando na porta ${PORT}`));
