3d131c14db2fc96bc9f884e0554c92561d3e4588
joao966 <joaooliveira966@gmail.com>
2021-10-12 15:24:02 -0300

    FIX

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (request, response) => {
+  response.send();
+});
+
 app.use(routes);
 // app.use(middleError);
 
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const app = require('./app');
 require('dotenv').config();
+const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || '3000';
 
 app.listen(PORT, () => console.log(`app rodando na porta ${PORT}`));
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
