5ebfff600b4d989edf72a278ae6abbdd51f45302
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-12 02:24:38 -0300

    making app use bodyParser.json() function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const middlewares = require('./middlewares');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
