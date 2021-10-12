5f434125ff288764221015f30a48e0f7003a3b0b
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-10-12 12:19:38 -0300

    create controller getUserByID

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,7 +28,24 @@ async function getAllUsers(_req, res) {
   }
 }
 
+async function getUserByID(req, res) {
+  try {
+    const { id } = req.params;
+
+    const user = await userService.getUserByID(id);
+    if (user === null) {
+      return res.status(404).json({ message: 'User does not exist' });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(`An unknown error has occurred: ${error}`);
+    return res.status(500).json({ message: 'An unknown error has occurred' });
+  }
+}
+
 module.exports = {
   create,
   getAllUsers,
+  getUserByID,
 };
