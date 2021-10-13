b736734f3846c6fce8217e605fb13afd8384a259
Mariana Savoldi <marianasavodi123@gmail.com>
2021-10-13 18:36:32 -0300

    Starting Sequelize and creating the config.js file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const app = express();
+require('dotenv').config();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const { PORT } = process.env;
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
