0de85cf84c06649fbfaaca8d9258a642aadf7ec2
Thiago Marchini <thiago@msn.com>
2021-10-13 17:27:39 -0300

    Requisito 6

--- controllers/categories.js
+++ controllers/categories.js
@@ -4,10 +4,14 @@ require('dotenv/config');
 
 const router = express.Router();
 
+router.get('/', async (_req, res) => {
+  const categories = await Category.findAll();
+  return res.status(200).json(categories);
+});
+
 router.post('/', async (req, res) => {
   const { name } = req.body;
   if (!name) return res.status(400).json({ message: '"name" is required' });
-  console.log(name);
   const newCategory = await Category.create({ name });
   return res.status(201).json(newCategory);
 });
