ac2c1afef6446a35d6c22141b1c1ffe10bba84b7
Leandro Reis <lleandrofr@gmail.com>
2021-10-13 09:38:00 -0300

    create a user without any validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,10 @@
 const { CREATED } = require('http-status');
+const { User } = require('../models');
 
-const createUser = async (req, res) => res.status(CREATED).json();
+const createUser = async (req, res) => {
+  const userData = await User.create(req.body);
+  res.status(CREATED).json(userData);
+};
 
 module.exports = {
   createUser,
