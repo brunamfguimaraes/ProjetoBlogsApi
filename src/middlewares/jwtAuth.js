3fc25e220aaae5f40ddcc9715add29aa244ff95a
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-10-13 15:51:52 -0300

    jwtAuth middleware error msg correction

--- src/middlewares/jwtAuth.js
+++ src/middlewares/jwtAuth.js
@@ -6,7 +6,7 @@ const jwtSecret = process.env.JWT_SECRET || 'test';
 module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
 
-  if (!token) return res.status(401).json({ message: 'Token not Found' });
+  if (!token) return res.status(401).json({ message: 'Token not found' });
 
   try {
     const { email, password } = jwt.verify(token, jwtSecret);
