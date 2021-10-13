d16a87864d78b0292e59fdbc7d34fe5d71af7e6d
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-10-13 10:37:00 -0300

    adicionando função que valida o tamanho mínimo da senha

---
+++ validations/validatePasswordMinLength.js
@@ -0,0 +1,14 @@
+const RequestError = require('../helper/customErrors');
+
+module.exports = (field = { fieldName: '', minLength: 0 }) => {
+  let message = '';
+
+  const [fieldKey, minLengthRequired] = Object.keys(field);
+  if (!message && field[fieldKey].length < field[minLengthRequired]) { 
+    message = `"${fieldKey}" length must be ${field[minLengthRequired]} characters long`; 
+  }
+
+  if (message) {
+    throw new RequestError('badRequest', message);
+  }
+};
