ee1183c8b15163937ddda1ca432b57390fd5378d
Mariana Savoldi <marianasavodi123@gmail.com>
2021-10-13 19:33:58 -0300

    Fix PORT 2

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
 const app = express();
-require('dotenv').config();
 
-const { PORT } = process.env || 3000;
-app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
+const port = 3000;
+app.listen(port, () => console.log(`ouvindo porta ${port}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
