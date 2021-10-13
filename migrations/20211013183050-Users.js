d055aa6939ac3f9b1fe0356bef89d01bb598b16a
RafaMaths <rmathscoach@gmail.com>
2021-10-13 15:54:05 -0300

    I create the 'user' migration

--- migrations/20211013183050-Users.js
+++ migrations/20211013183050-Users.js
@@ -2,20 +2,39 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'id',
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'displayName',
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+        field: 'email',
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'password',
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING,
+        field: 'image',
+      }
+    });
+  },  
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Users');
   }
 };
