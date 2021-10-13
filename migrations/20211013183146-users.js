e6069c8802e2216a226963b7d334802543b912db
esmaben <felippecorrea2010@gmail.com>
2021-10-13 16:00:52 -0300

    Dev estrutura sequlize: criando migrate de users

--- migrations/20211013183146-users.js
+++ migrations/20211013183146-users.js
@@ -2,20 +2,39 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    const userTable = queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      image: {
+        type: Sequelize.STRING,
+      }
+    });
+
+    return userTable;
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    queryInterface.dropTable("Users")
   }
 };
