768be625fe400f81c4a67a1360a9eb1f42d170ab
RafaMaths <rmathscoach@gmail.com>
2021-10-13 17:17:35 -0300

    I create the 'user' model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    userId: { type: DataTypes.INTEGER, primaryKey: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     image: DataTypes.INTEGER,
