68945cacc65418c629b6768d6bffb3d37f0bad20
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-10-13 17:11:51 -0300

    done: auth/createToken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
 const { User } = require('../models');
+const { newToken } = require('../auth/createToken');
 
 const createUser = async (req, res) => {
   const { body } = req;
-  const newUser = await User.create(body);
-  res.status(201).json({ newUser });
+  await User.create(body);
+  const token = await newToken(body);
+  res.status(201).json({ token });
 };
 
 module.exports = {
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,6 +5,7 @@ const { validUser, uniqueEmail } = require('../middlewares/userValidations');
 const router = express.Router();
 
 router.route('/')
-  .post(validUser, uniqueEmail, createUser);
+  .post(validUser, uniqueEmail, createUser)
+  .get();
 
 module.exports = router;
