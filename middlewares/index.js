5a890adacadf628c0c64e3587fc86488d5f5aa07
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-13 00:05:58 -0300

    Add validate token middleware in middleware index

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,10 +3,13 @@ const validatePasswordCreate = require('./validatePasswordCreate');
 const validateEmailCreate = require('./validateEmailCreate');
 const validatePasswordLogin = require('./validatePasswordLogin');
 const validateEmailLogin = require('./validateEmailLogin');
+const validateToken = require('./validateToken');
 
 module.exports = { 
   errorMiddleware,
   validatePasswordCreate,
   validateEmailCreate,
   validatePasswordLogin,
-  validateEmailLogin };
+  validateEmailLogin,
+  validateToken,
+ };
