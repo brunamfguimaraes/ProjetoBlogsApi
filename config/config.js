c00a6fd528c60292d422b4f8c21ebeee27cbb4cb
Caio <thehaseol@gmail.com>
2021-10-13 18:49:13 -0300

    recomeçando projeto

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv/config');
+
+module.exports = {
+  development: {
+    username: 'Caio',
+    password: '12345678',
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'blogs_api',
+    host: process.env.HOSTNAME,
+    dialect: 'mysql',
+  },
+};
