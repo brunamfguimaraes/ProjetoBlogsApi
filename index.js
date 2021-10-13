a7b66f3fdbdba6a6a8de65b5541b9c32631a0ddb
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-10-13 18:27:56 -0300

    Inicio da aplicação com sequelize init e criação do banco e tabelas por meio do sequelize

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Requisições que precisam de token mas não o receberam devem retornar um código de status 401;
+// Requisições que não seguem o formato pedido pelo servidor devem retornar um código de status 400;
+// Um problema inesperado no servidor deve retornar um código de status 500;
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
