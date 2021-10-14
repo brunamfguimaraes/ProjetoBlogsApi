432a10c73a2b865a0185580bc05656fe728c1d70
isTorres <ismaeltorres612@gmail.com>
2021-10-14 19:26:25 -0300

    fix token error msg

--- services/user.js
+++ services/user.js
@@ -31,14 +31,14 @@ const authenticationToken = (user) => {
 
 const validToken = async (req, res, next) => {
   const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ message: 'token not found' });
+  if (!token) return res.status(401).json({ message: 'Token not found' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'invalid token' });
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
 
