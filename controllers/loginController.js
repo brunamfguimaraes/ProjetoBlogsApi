1e8f171a136953e9eb2f45103dadde7ae07dba55
esmaben <felippecorrea2010@gmail.com>
2021-10-14 18:53:11 -0300

    Dev req2: criando validaçao de campos no loginService

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const statusCode = require('http-status-codes');
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+/* const { User } = require('../models'); */
+const loginService = require('../services/loginService');
 
 const router = express.Router();
 
@@ -9,7 +10,12 @@ const secret = 'seusecretdetoken';
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    const login = await User.findOne({ where: { email, password } });
+    const login = await loginService.loginUser({ email, password });
+
+    if (login.message) {
+        return res.status(statusCode.BAD_REQUEST).json({ message: login.message });
+    }
+
     const jwtConfig = {
         expiresIn: '7d',
         algorithm: 'HS256',
--- services/loginService.js
+++ services/loginService.js
@@ -1,2 +1,22 @@
 const { User } = require('../models');
 
+const validateField = (email, password) => {
+    if (!email) {
+        return { message: '"email" is required' };
+    }
+
+    if (!password) {
+        return { message: '"password" is required' };
+    }
+   
+    return true;
+};
+
+const loginUser = async ({ email, password }) => {
+    const validField = validateField(email, password);
+    if (validField !== true) return { message: validField.message };
+    const login = await User.findOne({ where: { email, password } });
+    return login;
+};
+
+module.exports = { loginUser };
