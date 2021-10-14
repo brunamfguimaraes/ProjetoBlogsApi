fbe3fa51149fad4ff82a6a4350373c239034976b
esmaben <felippecorrea2010@gmail.com>
2021-10-14 12:56:17 -0300

    Dev requisito 1: funcao userCreate criado no services e router.post criado nos controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const statusCode = require('http-status-codes');
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const userService = require('../services/userService');
 
 const router = express.Router();
 
 const secret = 'seusecretdetoken';
 
 router.post('/', async (req, res) => {
-    try {
-        const { displayName, email, password, image } = req.body;
-        const jwtConfig = {
-            expiresIn: '7d',
-            algorithm: 'HS256',
-        };
-        const createUser = await User.create({ displayName, email, password, image });
-        const token = jwt.sign({ data: createUser }, secret, jwtConfig);
-        return res.status(statusCode.CREATED).json({ token });
-    } catch (error) {
-        console.log(error);
-        return res.status(statusCode.INTERNAL_SERVER_ERROR)
-        .json({ message: 'Ops! algo deu errado' });
+    const { displayName, email, password, image } = req.body;
+
+    const createUser = await userService.createUser({ displayName, email, password, image });
+    
+    if (createUser.message) {
+        return res.status(400).json({ message: createUser.message });
     }
+
+    const jwtConfig = {
+        expiresIn: '7d',
+        algorithm: 'HS256',
+    };
+    const token = jwt.sign({ data: createUser }, secret, jwtConfig);
+    return res.status(statusCode.CREATED).json({ token });
 });
 
 module.exports = router;
+
+/* try {
+    const { displayName, email, password, image } = req.body;
+    const jwtConfig = {
+        expiresIn: '7d',
+        algorithm: 'HS256',
+    };
+    const createUser = await userService({ displayName, email, password, image });
+    const token = jwt.sign({ data: createUser }, secret, jwtConfig);
+    return res.status(statusCode.CREATED).json({ token });
+} catch (error) {
+    console.log(error);
+    return res.status(statusCode.INTERNAL_SERVER_ERROR)
+    .json({ message: 'Ops! algo deu errado' });
+} */
---
+++ services/userService.js
@@ -0,0 +1,55 @@
+const { User } = require('../models');
+
+const validateName = (displayName) => {
+    if (!displayName || displayName.length < 8) {
+        return false;
+    }
+
+    return true;
+};
+
+const validateField = (email, password) => {
+    if (!email) {
+        return email;
+    }
+    if (!password) {
+        return password;
+    }
+    return true;
+};
+
+const validateEmail = (email) => {
+    const regexEmail = /\b[\w.-]+@[\w.-]+.\w{2,4}\b/;
+    if (!regexEmail.test(email)) {
+        return false;
+    }
+    
+    return true;
+};
+
+const validatePassword = (password) => {
+    if (!password || password.length !== 6) {
+       return false; 
+    }
+
+    return true;
+};
+
+const createUser = async ({ displayName, email, password, image }) => {
+    const validName = validateName(displayName);
+    const validField = validateField(email, password);
+    const validEmail = validateEmail(email);
+    const validPassword = validatePassword(password);
+    const existEmail = await User.findOne({ email });
+    if (!validName) {
+        return { message: 'displayName length must be at least 8 characters long' };
+    }
+    if (!validField) { return { message: `${validField} is required` }; }
+    if (!validEmail) { return { message: 'email must be a valid email' }; }
+    if (!validPassword) { return { message: 'password length must be 6 characters long' }; }
+    if (existEmail) { return { message: 'User already registered' }; }
+    const user = await User.create({ displayName, email, password, image });
+    return user;
+};
+
+module.exports = { createUser };
