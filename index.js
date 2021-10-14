57966d87dc73854b044c91fede2fc1553629dabb
Emerson Saturnino <programador.emerson@hotmail.com>
2021-10-13 23:42:35 -0300

    Requisito 8 reupando

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { verifyCategory, getAllCategories,
 
 const { verifyCategoryId, verifyContent,
   verifyTitle, verifyCategoryIdExists, 
-  createBlogPost } = require('./services/post.js');
+  createBlogPost, getAllPosts } = require('./services/post.js');
 
 const app = express();
 
@@ -31,6 +31,6 @@ app.post('/post', validaToken, verifyCategoryId, verifyContent, verifyTitle,
 app.get('/user', validaToken, getAllUsers);
 app.get('/user/:id', validaToken, getUser);
 app.get('/categories', validaToken, getAllCategories);
-// app.get('/post', validaToken, getAllPosts);
+app.get('/post', validaToken, getAllPosts);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
