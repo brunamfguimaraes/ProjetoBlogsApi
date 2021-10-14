11499d8f442c22ba9e0699ea356eccbbfc2aeaf0
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-14 18:37:12 -0300

    cria tanto a migration quanto o model das tabelas blogpost e posts categories e renomeia os arquivos dos models

---
+++ models/BlogPost.js
@@ -0,0 +1,17 @@
+const BlogPostModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: 'user_id', as: 'users' });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPostModel;
--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const UserModel = (sequelize, DataTypes) => {
 
   User.associate = (models) => {
     User.hasMany(models.BlogPost, {
-      foreignKey: 'user_id', as: 'user',
+      foreignKey: 'user_id', as: 'blogPosts',
     });
   };
 
