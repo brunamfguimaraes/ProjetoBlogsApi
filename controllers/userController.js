df9e11ffbcc3fd00d23b9f4f1e86b6653d507ed7
RafaMaths <rmathscoach@gmail.com>
2021-10-14 18:06:07 -0300

    requirement 2: DONE

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,18 +37,16 @@ router.post('/login', async (req, res) => {
       algorithm: 'HS256',
     };
 
-    const login = await User.findOne({ email, password });
+    const login = await User.findOne({ where: { email, password } });
     console.log(login);
-    if (!login) {
-      return res.status(CODE.BAD_REQUEST).json({ message: 'Invalid fields' });
-    }
-
+    if (!login) return res.status(CODE.BAD_REQUEST).json({ message: 'Invalid fields' });
+     
     const token = jwt.sign({ data: login }, secret, jwtConfig);
 
     return res.status(CODE.OK).json({ token });
   } catch (error) {
     console.log(error.message);
-    res.status(CODE.NOT_FOUND).json({ message: 'It is a problem' });
+    res.status(CODE.NOT_FOUND).json({ message: 'ERROR' });
   }
 });
 
