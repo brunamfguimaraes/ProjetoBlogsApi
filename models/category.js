a005923907eb3def64b05855c6b89bb397e2366a
Mariana Savoldi <marianasavodi123@gmail.com>
2021-10-14 20:25:12 -0300

    Restarting the models (tables names)

--- models/category.js
+++
@@ -1,9 +0,0 @@
-const Category = (sequelize, DataTypes) => {
-  const category = sequelize.define('Category', {
-    name: DataTypes.STRING,
-  });
-
-  return category;
-};
-
-module.exports = Category;
