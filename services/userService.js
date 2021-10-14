333fde14f69af5944586aa83c7097a00e7008e20
Misael Bueno <misa.hbueno@gmail.com>
2021-10-13 21:12:27 -0300

    fixed lint

--- services/userService.js
+++ services/userService.js
@@ -22,31 +22,27 @@ const existsPasswordOrEmail = (email, password) => {
 
 const validateNameEmailPassword = (displayName, email, password) => {
   if (displayName.length < 8) {
-    const resp = {
-      message: '"displayName" length must be at least 8 characters long',
-      status: 400,
-    };
+    const resp = { message: '"displayName" length must be at least 8 characters long' };
     return resp;
   }
 
   const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (!emailRegex.test(email)) {
-    const resp = {
-      message: '"email" must be a valid email',
-      status: 400,
-    };
+    const resp = { message: '"email" must be a valid email' };
     return resp;
   }
 
   if (password.length < 6) {
-    const resp = {
-      message: '"password" length must be 6 characters long',
-      status: 400,
-    };
+    const resp = { message: '"password" length must be 6 characters long' };
     return resp;
   }
 
   return false;
+};
+
+const respEmailUnique = {
+  message: 'User already registered',
+  status: 409,
 };
 
 const createUser = async (displayName, email, password, image) => {
@@ -57,19 +53,14 @@ const createUser = async (displayName, email, password, image) => {
   }
 
   const validates = validateNameEmailPassword(displayName, email, password);
-
   if (validates.message) {
+    validates.status = 400;
     return validates;
   }
 
   const emailIsNotUnique = await User.findOne({ email });
-
   if (emailIsNotUnique) {
-    const resp = {
-      message: 'User already registered',
-      status: 409,
-    };
-    return resp;
+    return respEmailUnique;
   }
 
   const newUser = await User.create({ displayName, email, password, image });
