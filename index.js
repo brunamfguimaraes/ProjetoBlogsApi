c52ba852b806c2cc86ec2da03fb5a0678f9868f8
Emerson Saturnino <programador.emerson@hotmail.com>
2021-10-13 23:42:15 -0300

    Requisito 7 reupando

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ testeEmptyEmail, getUser } = require('./services/user.js');
 const { verifyCategory, getAllCategories,
   createCategory } = require('./services/categories.js');// , getAllCategories } = require('./services/categories.js');
 
-// const { verifyCategoryId, verifyContent,
-//   verifyTitle, verifyCategoryIdExists, 
-//   createBlogPost, getAllPosts } = require('./services/post.js');
+const { verifyCategoryId, verifyContent,
+  verifyTitle, verifyCategoryIdExists, 
+  createBlogPost } = require('./services/post.js');
 
 const app = express();
 
@@ -25,8 +25,8 @@ app.get('/', (request, response) => {
 app.post('/user', checkEmail, checkEmailExists, checkPassword, checkName, createUser);
 app.post('/login', testeEmptyEmail, testeEmptyPassword, userLogin);
 app.post('/categories', validaToken, verifyCategory, createCategory);
-// app.post('/post', validaToken, verifyCategoryId, verifyContent, verifyTitle,
-//   verifyCategoryIdExists, createBlogPost);
+app.post('/post', validaToken, verifyCategoryId, verifyContent, verifyTitle,
+  verifyCategoryIdExists, createBlogPost);
 
 app.get('/user', validaToken, getAllUsers);
 app.get('/user/:id', validaToken, getUser);
