d3360f947065e2eb1a0a8eea564f948be6c652e0
Misael Bueno <misa.hbueno@gmail.com>
2021-10-14 10:38:52 -0300

    fixed test1

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.json());
 
 app.use('/user', userController);
 
-const PORT = process.env.PORT || 3000;
-// const PORT = 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
