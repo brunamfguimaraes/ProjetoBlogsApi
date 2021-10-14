c379000517886ed3bb050471ff44e8acbf4e79ad
esmaben <felippecorrea2010@gmail.com>
2021-10-14 19:12:33 -0300

    Dev req02: criando validaçao caso passe campo vazio

--- services/loginService.js
+++ services/loginService.js
@@ -12,8 +12,22 @@ const validateField = (email, password) => {
     return true;
 };
 
+const notEmpty = (email, password) => {
+   if (email === '') {
+      return { message: '"email" is not allowed to be empty' };
+   } 
+
+   if (password === '') {
+      return { message: '"password" is not allowed to be empty' };
+   }
+
+   return true;
+};
+
 const loginUser = async ({ email, password }) => {
     const validField = validateField(email, password);
+    const validEmpty = notEmpty(email, password);
+    if (validEmpty !== true) return { message: validEmpty.message };
     if (validField !== true) return { message: validField.message };
     const login = await User.findOne({ where: { email, password } });
     return login;
