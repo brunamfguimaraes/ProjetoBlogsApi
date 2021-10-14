e01e2dc16f74f1349643bfdfddac5b725a9a6c6b
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-14 17:13:14 -0300

    cria a migraçao e o model da tabela user, ja referenciando o id como foreign key para a tabela posts

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: { type: DataTypes.STRING, unique: true },
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id', as: 'user',
+    });
+  };
+
+  return User;
+};
+
+module.exports = UserModel;
