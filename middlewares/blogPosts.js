9bea32131f5b24c8e87adabc4ac2e61b5030ac32
Caio <thehaseol@gmail.com>
2021-10-13 22:43:47 -0300

    fix do blogPosts v3

--- middlewares/blogPosts.js
+++ middlewares/blogPosts.js
@@ -8,9 +8,9 @@ const blogPostSchema = Joi.object().keys({
   title: Joi.string().not().empty().required(),
 
   content: Joi.string().not().empty().required(),
-
-  categoryIds: Joi.array().items(Joi.number()).not().empty().required(),
   
+  categoryIds: Joi.array().items(Joi.number()).not().empty()
+  .required(),
 });
 
 const validatePost = (req, res, next) => {
