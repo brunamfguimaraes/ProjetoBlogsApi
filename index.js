6e2d8c929822500d5c61228ea2c43b2cf13ada5e
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-10-13 21:50:20 -0300

    Req 1 a 4

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-require('dotenv');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const { checkEmailExists, testeEmptyEmail, userLogin,
   checkPassword, checkName, checkEmail, createUser,
   testeEmptyPassword, validaToken, getAllUsers, getUser } = require('./services/user.js');
 
-// const { verifyCategory,
-//   createCategory, getAllCategories } = require('./services/categories.js');
+// const { verifyCategory, createCategory, getAllCategories } = require('./services/categories.js');
 
-// const { verifyCategoryId, verifyContent,
-//   verifyTitle, verifyCategoryIdExists, 
-//   createBlogPost, getAllPosts } = require('./services/post.js');
+// const { verifyCategoryId, verifyContent, verifyTitle, verifyCategoryIdExists, createBlogPost, getAllPosts } = require('./services/post.js');
 
 const app = express();
 
@@ -25,12 +20,11 @@ app.get('/', (request, response) => {
 app.post('/user', checkEmail, checkEmailExists, checkPassword, checkName, createUser);
 app.post('/login', testeEmptyEmail, testeEmptyPassword, userLogin);
 // app.post('/categories', validaToken, verifyCategory, createCategory);
-// app.post('/post', validaToken, verifyCategoryId, verifyContent, verifyTitle,
-  // verifyCategoryIdExists, createBlogPost);
+// app.post('/post', validaToken, verifyCategoryId, verifyContent, verifyTitle, verifyCategoryIdExists, createBlogPost);
 
 app.get('/user', validaToken, getAllUsers);
 app.get('/user/:id', validaToken, getUser);
 // app.get('/categories', validaToken, getAllCategories);
 // app.get('/post', validaToken, getAllPosts);
 
-app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}!`));
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
