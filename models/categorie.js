b44968c0e511ee69ed0e9acb3ab60a6070f63262
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-14 17:28:51 -0300

    cria o Model e o Migrate para a tabela Categories

---
+++ models/categorie.js
@@ -0,0 +1,11 @@
+const CategorieModel = (sequelize, DataTypes) => {
+  const Categorie = sequelize.define('Categorie', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+  });
+  return Categorie;
+};
+
+module.exports = CategorieModel;
