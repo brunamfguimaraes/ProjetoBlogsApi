9be63784af37e412edb5b52c583894f0c7756d91
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-14 19:06:59 -0300

    Done: MSC architecture created

---
+++ api/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// const cors = require('cors');
+// const studentsRoutes = require('../routers/studentsRoutes');
+
+const app = express();
+// const corsOptions = {
+//   origin: 'http://localhost:8080',
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
+
+// app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use('/students');
+
+module.exports = app;
