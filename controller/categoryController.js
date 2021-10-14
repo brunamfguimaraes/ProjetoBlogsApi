b22995abeb2376962883d2611f2143d89bb1ead7
tonpasso <wellpasso@gmail.com>
2021-10-14 08:06:42 -0300

    requisito 06

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -20,6 +20,18 @@ categoryRouter.post('/', verifyToken, categoryValidate, async (req, res) => {
   }
 });
 
+categoryRouter.get('/', verifyToken, async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+    res.status(200).json(categories);  
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: 'invalid data',
+    });   
+  }
+});
+
 // validateUser, createToken,
 
 module.exports = categoryRouter;
--- services/loginService.js
+++ services/loginService.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 const checkEmailUser = async (email) => {
   const user = await User.findOne({ where: { email } });
 
-  if (!user) return { fieldError: true, message: 'Invaid fields' };
+  if (!user) return { fieldError: true, message: 'Invalid fields' };
 
   return { isError: false };
 };
