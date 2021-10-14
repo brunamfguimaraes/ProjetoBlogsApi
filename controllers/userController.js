fca92eb5c46b666f223add98ce46456531281a8a
Misael Bueno <misa.hbueno@gmail.com>
2021-10-14 10:28:48 -0300

    fixed lint

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,6 @@ router.post('/', async (req, res) => {
     const token = jwt.sign({ data: newUser }, secret, jwtConfig);
   
     return res.status(201).json({ token });
-
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
