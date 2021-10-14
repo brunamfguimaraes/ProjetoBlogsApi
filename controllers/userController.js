19bfdcf88cd6382229f04f70e0bf72be7c8b32cf
Misael Bueno <misa.hbueno@gmail.com>
2021-10-14 17:07:13 -0300

    req01 return fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// require('dotenv').config();
+require('dotenv').config();
 
 const router = express.Router();
 const jwt = require('jsonwebtoken');
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
     const newUser = await userService.createUser(displayName, email, password, image);
 
     if (newUser.message) {
-      res.status(newUser.status).json({ message: newUser.message });
+      return res.status(newUser.status).json({ message: newUser.message });
     }
 
     const jwtConfig = {
@@ -30,7 +30,7 @@ router.post('/', async (req, res) => {
     // return res.status(201).json({ newUser });
   } catch (e) {
     console.log(e.message);
-    res.status(500).json({ message: 'Algo deu errado' });
+    return res.status(500).json({ message: 'Algo deu errado' });
   }
 });
 
