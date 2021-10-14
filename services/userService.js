34d961079140e44fa4b26fb91b9566c4f02ebd7f
esmaben <felippecorrea2010@gmail.com>
2021-10-14 15:29:33 -0300

    Dev req1: tirando comentarios desnecessarios no arquivo userService

--- services/userService.js
+++ services/userService.js
@@ -60,22 +60,10 @@ const existingEmail = async (email) => {
 };
 
 const createUser = async ({ displayName, email, password, image }) => {
-  /*   const validName = validateName(displayName);
-    const validField = validateField(email, password);
-    const validEmail = validateEmail(email);
-    const validPassword = validatePassword(password); */
     const validations = allValidations(displayName, email, password);
     const existUser = await existingEmail(email);
-    
- /*    if (!validName) {
-        return { message: 'displayName length must be at least 8 characters long' };
-    }
-    if (validField !== true) return { message: validField.message };
-    if (!validEmail) { return { message: 'email must be a valid email' }; }
-    if (!validPassword) { return { message: 'password length must be 6 characters long' }; } */
     if (validations.message) { return { message: validations.message }; }
     if (existUser.message) { return { message: existUser.message }; }
-
     const user = await User.create({ displayName, email, password, image });
     return user;
 };
