f571723cb28b0b44af074fa142d8aabac7916c67
Caio <thehaseol@gmail.com>
2021-10-13 21:33:24 -0300

    Adiçao de enpoint Post Categories

--- migrations/20211014002637-create-categories.js
+++ migrations/20211014002637-create-categories.js
@@ -1,23 +1,14 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable('categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      displayName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      image: {
+      name: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -31,6 +22,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('categories');
   }
 };
---
+++ models/categories.js
@@ -0,0 +1,15 @@
+const defineCategoryModels = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Categories',
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    }
+  );
+
+  return Category;
+};
+
+module.exports = defineCategoryModels;
