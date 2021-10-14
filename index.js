cd947594f7e9cae324f6844a1ac72e4f6019317b
André Arnoni <andre_arnoni@yahoo.com.br>
2021-10-14 19:04:36 -0300

    executado requisito 4

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ const {
   emailExists,
   validateFields } = require('./middlewares/validationFields');
 
+  app.get('/user/:id', validationJWT, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      if (!user) return res.status(404).json(messages.USER_DOES_NOT_EXIST);
+
+      return res.status(200).json(user);
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    }
+  });
+
   app.get('/user', validationJWT, async (_req, res) => {
     try {
       const users = await User.findAll();
