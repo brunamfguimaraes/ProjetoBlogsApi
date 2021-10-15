79bd91145af1d27274a5d84925d2e3b1870c2513
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-10-15 18:27:21 -0300

    BlogPost - Adicionando alteraçao teste

--- models/BlogPosts.js
+++ models/BlogPosts.js
@@ -5,10 +5,9 @@ const createBlogPost = (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: { type: DataTypes.INTEGER, foreignKey: true },
-    // published: DataTypes.DATE,
-    // updated: DataTypes.DATE,
-    
-  }, { timestamps: true, createdAt: 'published', updatedAt: 'updated' });
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, { timestamps: false, tableName: 'BlogPosts' });
   BlogPost.associate = (models) => {
     BlogPost.belongsTo(models.User, 
     {
