5c2bdec7736215887b69f8cbb4ebd72107dc35a0
Paulo Xavier <pxavierf@gmail.com>
2021-10-15 09:23:38 -0300

    criando models e migrations de postscategories

--- controllers/BlogPostController.js
+++ controllers/BlogPostController.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const { BlogPost } = require('../models');
+const { BlogPost, PostsCategory } = require('../models');
 const validateJWT = require('../middlewares/token/validateJWT');
 const postValidate = require('../middlewares/postValidate');
 
 const BlogPostRouter = express.Router();
 
 BlogPostRouter.post('/', validateJWT, postValidate, async (req, res) => {
-  const { title, content } = req.body;
+  const { title, content, categoryIds } = req.body;
   const { user } = req;
 
   const post = await BlogPost.create({ userId: user.id, title, content });
+  
+  const { id } = post;
+
+  await categoryIds.forEach(async (categoryId) => {
+    const postId = id;
+    await PostsCategory.create({ categoryId, postId });
+  });
+
   const { published, updated, ...postWithoutTimes } = post.dataValues;
 
   return res.status(201).json(postWithoutTimes);
