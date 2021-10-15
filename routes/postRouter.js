09449c675c65bf29e228be5fba36663f5bc4a882
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-10-15 18:34:46 -0300

    ERRO - Removendo alteraçoes

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -5,7 +5,6 @@ const { validToken } = require('../api/auth/validToken');
 const { validPost, validCategoryIds } = require('../middlewares/postValidations');
 
 const router = express.Router();
-// help
 
 router.route('/')
 .post(validToken, validPost, validCategoryIds, postController.add)
