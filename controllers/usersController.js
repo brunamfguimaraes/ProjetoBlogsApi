d2afac1194e3a8bcd2e39a543d21211869f2d856
Daniel <dsouzaborini@gmail.com>
2021-10-15 16:39:55 -0300

    add login options on usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,6 +6,13 @@ const createUser = async (req, res) => {
   return res.status(newUser.resp.status).json(newUser.resp.content);
 };
 
+const loginUser = async (req, res) => {
+  const authUser = await userServices.loginUser(req.body);
+  if (authUser.err) return res.status(authUser.err.status).json(authUser.err.message);
+  return res.status(authUser.resp.status).json(authUser.resp.content);
+};
+
 module.exports = {
   createUser,
+  loginUser,
 };
