cae5179d749d854991497672a7cffead28ae15b7
aythan azevedo <aythan16@gmail.com>
2021-10-15 15:56:44 -0300

    recode migrations and creat blog post

--- migrations/  20211010142824-create-posts-categorie.js
+++ migrations/  20211010142824-create-posts-categorie.js
@@ -1,28 +1,62 @@
-'use strict';
+// 'use strict';
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     await queryInterface.createTable('PostsCategories', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       title: {
+//         type: Sequelize.STRING
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       }
+//     });
+//   },
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.dropTable('PostsCategories');
+//   }
+// };
+
+// cole esse código dentro do arquivo da migration "user-books"
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PostsCategories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('PostsCategories');
-  }
-};
-20211010135607-create-blog-post.js
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.createTable('PostsCategories', {
+        postId: {
+          type: Sequelize.INTEGER,
+          field: 'id',
+          references: {
+            model: 'BlogPosts',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          primaryKey: true,
+        },
+        categoryId: {
+          type: Sequelize.INTEGER,
+          field: 'id',
+          references: {
+            model: 'Categories',
+            key: 'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          primaryKey: true,
+        },
+      });
+    },
+  
+    down: async (queryInterface, _Sequelize) => {
+      await queryInterface.dropTable('PostsCategories');
+    },
+  };
---
+++ models/blogPost.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+      city: DataTypes.STRING,
+      street: DataTypes.STRING,
+      number: DataTypes.INTEGER,
+      userId: { type: DataTypes.INTEGER, foreignKey: true },
+      // A declaração da Foreign Key é opcional no model
+    },
+    {
+      timestamps: false,
+      tableName: 'BlogPosts',
+      underscored: true,
+    });
+  
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.user,
+        { foreignKey: 'id', as: 'users' });
+    };
+  
+    return BlogPost;
+  };
