cba59cf81e0ce5605dfd7e2596b200eb3a036b3f
Jessie <miss_sc_jessi@hotmail.com>
2021-10-14 22:19:39 -0300

    req 4 lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { user, login, getUsers,findUser } = require('./controllers/userController');
+const { user, login, getUsers, findUser } = require('./controllers/userController');
 const validateJWT = require('./auth/validateJWT');
 
 const app = express();
