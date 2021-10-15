327819116d34397986697dde6f03cd3cd1920310
tonpasso <wellpasso@gmail.com>
2021-10-15 10:56:04 -0300

    requisitos 08 e 09

--- controller/blogPostController.js
+++ controller/blogPostController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { BlogPost, PostsCategory } = require('../models');
+const { BlogPost, PostsCategory, User, Category } = require('../models');
 const verifyToken = require('../middleware/verifyToken');
 const blogPostValidate = require('../middleware/blogPostValidate');
 const blogPostService = require('../services/blogPostService');
@@ -30,4 +30,52 @@ blogPostRouter.post('/', verifyToken, blogPostValidate, async (req, res) => {
       res.status(500).json({ error });
    }    
 });
+
+blogPostRouter.get('/', verifyToken, async (req, res) => {
+    try {
+        const post = await BlogPost.findAll({
+          include: [
+            { model: User, as: 'user', attributes: { exclude: ['password'] } },
+            { model: Category, as: 'categories', through: { attributes: [] } },
+          ],
+    });
+    
+    return res.status(200).json(post);        
+    } catch (error) {
+        res.status(500).json({ error });        
+    }
+});
+
+blogPostRouter.get('/:id', verifyToken, async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const post = await blogPostService.checkBlogPost(id);
+
+        if (post.fieldError) {
+            return res.status(404).json({ message: post.message });
+        }
+
+        const postBlog = await BlogPost.findByPk(id, {
+            include: [
+                { model: User, as: 'user', attributes: { exclude: ['password'] } },
+                { model: Category, as: 'categories', through: { attributes: [] } },
+            ],
+        });
+    
+      return res.status(200).json(postBlog);            
+    } catch (error) {
+        res.status(500).json({ error });        
+    }
+});
+
+// blogPostRouter.put('/', verifyToken, async (req, res) => {
+//     try {
+//         const 
+        
+//     } catch (error) {
+        
+//     }
+// })
+
 module.exports = blogPostRouter;
--- services/blogPostService.js
+++ services/blogPostService.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models');
+const { Category, BlogPost } = require('../models');
 
 const checkCategoryId = async (categoryIds) => {  
   const idCheck = await Promise.all(categoryIds.map((catId) => Category.findByPk(catId)));
@@ -10,6 +10,16 @@ const checkCategoryId = async (categoryIds) => {
   return { fieldError: false };
 };
 
+const checkBlogPost = async (id) => {
+  const blogId = await BlogPost.findByPk(id);
+
+  if (!blogId) {
+    return { fieldError: true, message: 'Post does not exist' };
+  }
+  return blogId;
+};
+
 module.exports = {
   checkCategoryId,
+  checkBlogPost,
 };
