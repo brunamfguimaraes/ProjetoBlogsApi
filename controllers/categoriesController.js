533151ac93578c467735ddf477b05b7089533cc9
esmaben <felippecorrea2010@gmail.com>
2021-10-15 15:38:46 -0300

    Dev req05: refatorando arquivo categoriesController.js

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const statusCode = require('http-status-codes');
 /* const jwt = require('jsonwebtoken'); */
-const { Categorie } = require('../models');
+/* const { Categorie } = require('../models'); */
+const { createCategory } = require('../services/categoriesService');
 
 const router = express.Router();
 
 router.post('/categories', async (req, res) => {
     const { name } = req.body;
-    const categories = await Categorie.create({ name });
+    const categories = await createCategory({ name });
     return res.status(statusCode.CREATED).json(categories);
 });
 
