039845b2456ddb8363969dead3f0f912142dc34f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-15 15:27:15 -0300

    Feat: Helper de erros

---
+++ helpers/errors.js
@@ -0,0 +1,12 @@
+const { StatusCodes } = require('http-status-codes');
+
+const error = (code, message) => ({
+  isError: true,
+  code,
+  message,
+});
+
+module.exports = {
+  badRequest: (message) => error(StatusCodes.BAD_REQUEST, message),
+  conflict: (message) => error(StatusCodes.CONFLICT, message),
+};
