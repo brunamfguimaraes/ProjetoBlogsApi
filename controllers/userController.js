eba886eed968e9a1ab90caf67fae76a52c0add96
Breno Rodrigues <brenorm@gmail.com>
2021-10-15 01:08:30 -0300

    Requisito 1: Criação de rota POST User + Controller

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { User } = require('../models');
+
+const userRouter = express.Router();
+
+// ---------------------------------------------------------------
+// Requisito 1: CONTROLLER responsável por receber a requisição de cadastro de usuário, chamar SERVICE e retornar o usuário cadastrado.
+
+userRouter.post('/', async (req, res) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+    const newUser = await User.create({ displayName, email, password, image });
+
+    return res.status(201).json(newUser);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+// ---------------------------------------------------------------
+
+module.exports = { userRouter };
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { userRouter } = require('./controllers/userController');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
@@ -8,3 +12,10 @@ app.listen(3000, () => console.log('ouvindo porta 3000!'));
 app.get('/', (request, response) => {
   response.send();
 });
+
+// ------------------------------------------------------------------
+// Requisitos 1 Rota de Users
+
+app.use('/user', userRouter);
+
+// ------------------------------------------------------------------
