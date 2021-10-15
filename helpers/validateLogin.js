7028f619d9e11a4e6e4fe27e1adb6d8466401c75
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-15 17:52:29 -0300

    Feat: Helper para validação do login

---
+++ helpers/validateLogin.js
@@ -0,0 +1,10 @@
+const Joi = require('@hapi/joi');
+
+const schemaLogin = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+module.exports = {
+  schemaLogin,
+};
