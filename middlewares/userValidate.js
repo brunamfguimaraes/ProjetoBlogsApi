4709ad975885da6aeff0b615fe46d1418cc11d9d
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-15 15:29:55 -0300

    Feat: Middlewares que valida dados

---
+++ middlewares/userValidate.js
@@ -0,0 +1,16 @@
+const { schemaUser } = require('../helpers/validateUser');
+const Error = require('../helpers/errors');
+
+const userValidate = (req, res, next) => {
+  const { error } = schemaUser.validate(req.body);
+  console.log(error);
+  const { code } = Error.badRequest();
+  if (error) {
+    return res.status(code).json({ 
+       message: error.details[0].message,
+    });
+  }
+  next();
+};
+
+module.exports = userValidate;
