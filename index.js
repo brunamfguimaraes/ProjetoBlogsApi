ae7f04c7dd6e56e574dc5076bfcd8ad026c89d6c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-15 15:25:54 -0300

    Feat: Add body-parse e controller user

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParse = require('body-parser');
+
+const userController = require('./controllers/userController');
 
 const app = express();
+app.use(bodyParse.json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
@@ -8,3 +12,5 @@ app.listen(3000, () => console.log('ouvindo porta 3000!'));
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/user', userController);
