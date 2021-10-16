440d9d2b2dd6819ac58d15c8dcc4f48b1b2ece7f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-16 16:16:22 -0300

    Feat: Cria model blog posts

---
+++ models/BlogPosts.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPosts = sequelize.define('BlogPosts', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, { timestamps: false });
+  
+  BlogPosts.associate = (models) => {
+    BlogPosts.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+  return BlogPosts;
+};
