57b6c7f0ab2b6e0327194a525b653ea79da9f730
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-10-16 11:26:42 -0300

    BUG - comentando a validaçao de categoryIds

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const postController = require('../controllers/postController');
 const { validToken } = require('../api/auth/validToken');
-const { validPost, validCategoryIds } = require('../middlewares/postValidations');
-
+const { validPost } = require('../middlewares/postValidations');
+// validCategoryIds
 const router = express.Router();
-
+// validCategoryIds
 router.route('/')
-.post(validToken, validPost, validCategoryIds, postController.add)
+.post(validToken, validPost, postController.add)
 .get(validToken, postController.findAll);
 
 module.exports = router;
