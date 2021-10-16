baa279a62d5c4231ca9df7c926d00ce33ca53b04
isTorres <ismaeltorres612@gmail.com>
2021-10-15 23:25:56 -0300

    fix again

--- services/user.js
+++ services/user.js
@@ -64,7 +64,7 @@ const getUsers = async (req, res) => {
 
 const getUserById = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findByPk({ where: { id } });
+  const user = await User.findByPk(id, { attributes: { exclude: ['password'] } });
   if (!user) return res.status(404).josn({ message: 'User does not exist' });
   return res.status(200).json(user);
 };
