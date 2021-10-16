53c48404fd500436f80058a8e8049df8d092c054
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-10-16 11:34:26 -0300

    BUG - comentando a validaçao de post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const postController = require('../controllers/postController');
 const { validToken } = require('../api/auth/validToken');
-const { validPost } = require('../middlewares/postValidations');
+// const { validPost } = require('../middlewares/postValidations');
 // validCategoryIds
 const router = express.Router();
-// validCategoryIds
+// validCategoryIds, validPost
 router.route('/')
-.post(validToken, validPost, postController.add)
+.post(validToken, postController.add)
 .get(validToken, postController.findAll);
 
 module.exports = router;
