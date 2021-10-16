4fa2ea22207074f69ad2efb5236d2e12b004822c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-10-16 09:17:51 -0300

    Feat: Controller que busca todas categorias

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,16 +3,23 @@ const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
 
 const categoriesRouter = express.Router();
-const loginValidate = require('../middlewares/validateJWT');
+const validateJWT = require('../middlewares/validateJWT');
 const categoriesValidate = require('../middlewares/categorieValidate');
-const { createCategories } = require('../services/categoriesService');
+const { createCategories, allCategories } = require('../services/categoriesService');
 
 categoriesRouter.post('/',
   categoriesValidate,
-  loginValidate,
+  validateJWT,
   rescue(async (req, res) => {
     const category = await createCategories(req.body);
     return res.status(StatusCodes.CREATED).json(category);
   }));
+
+  categoriesRouter.get('/',
+    validateJWT,
+    rescue(async (req, res) => {
+      const categories = await allCategories();
+      return res.status(StatusCodes.OK).json(categories);
+    }));
 
 module.exports = categoriesRouter;
