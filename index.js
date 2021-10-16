e154c77e080ff5d609d2ef1fd9f2ee4c8e68fb45
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-10-16 20:37:28 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+// não remova esse endpoint, e para o avaliador funcionar
+
+app.get('/', (request, response) => {
+  response.send();
+});
 
 const userRouter = require('./routers/userRouter');
 
 app.use('/user', userRouter);
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
