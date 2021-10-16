fe7b705a8dc1f0e9daac0b38aa6b4f13272017b8
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-16 03:18:39 -0300

    Fix: errors of EsLint

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { createUser } = require('../controllers/usersController');
 const validCreateUser = require('../middleware/validCreateUser');
 
-const validateJWT = require('../middleware/validateJWT');
+// const validateJWT = require('../middleware/validateJWT');
 
 const router = express.Router();
 
--- services/usersServices.js
+++ services/usersServices.js
@@ -1,4 +1,4 @@
-const { StatusCodes } = require('http-status-codes');
+// const { StatusCodes } = require('http-status-codes');
 const { User } = require('../models');
 const isValidEmail = require('../validations/isValidEmail');
 const checkIfTheUserExists = require('../validations/checkIfTheUserExists');
@@ -9,7 +9,7 @@ const createUserServices = async ({ displayName, email, password, image }) => {
     return checkIfTheUserExists(email, password); 
   }
 
-  const a = await User.create({ displayName, email, password, image });
+  await User.create({ displayName, email, password, image });
   return { isError: false };
 };
 
