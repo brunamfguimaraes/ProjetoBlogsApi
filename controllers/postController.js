dde26e05d6aa7d3702820dccef5819f63b7f854a
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-10-16 12:12:07 -0300

    BUG - Reformulando categoryIds - [DONE]

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,14 +1,15 @@
 const rescue = require('express-rescue');
-const { BlogPost } = require('../models');
-// PostsCategorie
+const { BlogPost, PostsCategorie } = require('../models');
+
 const add = rescue(async (req, res) => {
-  const { title, content } = req.body;
-  // categoryIds
+  const { title, content, categoryIds } = req.body;
   const { payload } = req;
   const newCategory = await BlogPost.create({ title, content, userId: payload.id });
   // categoryIds.forEach(async (categoryId) => PostsCategorie.create(
   //   { postId: payload.id, categoryId },
   // ));
+  categoryIds.map(async (categoryId) => PostsCategorie.create({ postId: payload.id, categoryId }));
+
   return res.status(201).json(newCategory);
 });
 
