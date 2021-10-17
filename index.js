44cde54d5b6ec5a5cf51ccc330ea65aa7059681e
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-17 00:41:15 -0400

    req 7

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const { verifyEmptyEmail, verifyEmptyPassword, userLogin, checkEmailExists, 
-  checkPassword, 
+const { verifyEmptyEmail, verifyEmptyPassword, userLogin, checkEmailExists,
+  checkPassword,
   checkName, checkEmail, createUser, validateToken, getAllUsers,
   getUser } = require('./services/user.js');
 const { verifyCategory, createCategory, getAllCategories } = require('./services/categories');
+const { verifyTitle, verifyContent, verifyCategoryId,
+  verifyCategoryIdExists, createPostBlog } = require('./services/post');
 
 const app = express();
 
@@ -19,6 +21,8 @@ app.get('/', (request, response) => {
 app.post('/user', checkEmail, checkEmailExists, checkPassword, checkName, createUser);
 app.post('/login', verifyEmptyEmail, verifyEmptyPassword, userLogin);
 app.post('/categories', validateToken, verifyCategory, createCategory);
+app.post('/post', validateToken, verifyTitle, verifyContent, verifyCategoryId,
+  verifyCategoryIdExists, createPostBlog);
 
 app.get('/user', validateToken, getAllUsers);
 app.get('/user/:id', validateToken, getUser);
