b2e81f864edd9bcb238bf854c82ebb7cb0b21710
CIGR2021 <appencigr@gmail.com>
2021-10-17 11:41:05 -0300

    criei o controller/user

---
+++ controller/user.js
@@ -0,0 +1,22 @@
+const { StatusCodes: {
+  BAD_REQUEST,
+  CONFLICT,
+  CREATED,
+} } = require('http-status-codes');
+const { createUser } = require('../services/user');
+const { token } = require('../auth/token');
+
+const user = async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+  const create = await createUser(displayName, email, password, image);
+
+  if (create.error) return res.status(BAD_REQUEST).json({ message: create.err.message });
+  if (create.error2) return res.status(CONFLICT).json({ message: 'User already registered' });
+
+  const newToken = await token(req.body);
+  return res.status(CREATED).json({ token: newToken });
+};
+
+module.exports = {
+  user,
+};
