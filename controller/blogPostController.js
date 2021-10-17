ee06e297cb0caca796e4e05b5822088c0357f08d
Cristian Bugs <cristian.bugs@gmail.com>
2021-10-17 20:41:00 -0300

    commented validation

--- controller/blogPostController.js
+++ controller/blogPostController.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { titleIsRequired, contentIsRequired } = require('../middleware/infoValidationPost');
-const { categoryIdIsRequired } = require('../middleware/infoValidationPost');
+// const { titleIsRequired, contentIsRequired } = require('../middleware/infoValidationPost');
+// const { categoryIdIsRequired } = require('../middleware/infoValidationPost');
 // const { idCategoryIsRequired } = require('../middleware/infoValidationPost');
 
-const { tokenValidation } = require('../middleware/infoValidationUser');
+// const { tokenValidation } = require('../middleware/infoValidationUser');
 const { getAllBlogPosts } = require('../service/postService');
 
 const router = express.Router();
 
 router.post('/',
-titleIsRequired,
-contentIsRequired,
-categoryIdIsRequired,
-tokenValidation,
+// titleIsRequired,
+// contentIsRequired,
+// categoryIdIsRequired,
+// tokenValidation,
 // idCategoryIsRequired,
 rescue(async (req, res) => {
     res.status(200).json('ok');
