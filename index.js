0207f9db0e114e1407f7b1cd7a1d0b27fcabdd39
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-16 21:23:30 -0400

    req 3

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { verifyEmptyEmail, verifyEmptyPassword, userLogin, checkEmailExists, 
-  checkPassword, checkName, checkEmail, createUser, validateToken, getAllUsers } = require('./services/user.js');
+  checkPassword, 
+  checkName, checkEmail, createUser, validateToken, getAllUsers } = require('./services/user.js');
 
 const app = express();
 
