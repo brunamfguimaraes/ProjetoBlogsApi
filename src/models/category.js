488156d93f1b2ef6782bd0e8c9c9d227e7a3d235
Joao Vanelli <vanellijoao@gmail.com>
2021-10-18 03:36:36 -0300

    adding Categories model and migration

---
+++ src/models/category.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    underscore: true,
+    tableName: 'Categories',
+  });
+
+  return category;
+};
