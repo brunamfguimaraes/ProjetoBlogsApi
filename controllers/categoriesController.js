52738242bc739a78cb70d8c69a7eb0f77ac984e9
root <diegoserafim1@gmail.com>
2021-10-18 15:53:58 -0300

    criado os endpoints get e post de categories

---
+++ controllers/categoriesController.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { Categories } = require('../models');
+require('dotenv/config');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const allCategory = await Categories.findAll();
+  return res.status(200).json({ allCategory });
+});
+
+router.post('/', async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  const createCategory = await Categories.create({ name });
+  return res.status(201).json(createCategory);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const userController = require('./controllers/userController');
 const loginController = require('./controllers/loginController');
+const categoriesController = require('./controllers/categoriesController');
 const { required, isEmpty } = require('./middlewares/loginMiddleware');
+const { validToken } = require('./middlewares/tokenMiddleware');
 
 const app = express();
 app.use(express.json());
 
 app.use('/user', userController);
 app.use('/login', required, isEmpty, loginController);
-// app.use('/');
+app.use('/categories', validToken, categoriesController);
 
 const port = process.env.PORT || 3000;
 
