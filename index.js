ae328c84fadbc2ec0f5f5dc641392ebc4f750846
Joao Vanelli <vanellijoao@gmail.com>
2021-10-18 03:45:45 -0300

    using the Categorie route

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Users, Login } = require('./src/routes');
+const { Users, Login, Categories } = require('./src/routes');
 
 const app = express();
 app.use(express.json());
@@ -10,6 +10,7 @@ app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 app.use('/user', Users);
 app.use('/login', Login);
+app.use('/categories', Categories);
 
 // do not remove that endpoint, it is used by the evatuator
 app.get('/', (request, response) => {
