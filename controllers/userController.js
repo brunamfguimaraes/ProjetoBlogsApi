a912aa85cd145d46de7475fe391f8f60eafa6dc7
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-10-19 15:34:25 -0300

    corrige req1

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,9 @@ const jwt = require('jsonwebtoken');
 const express = require('express');
 const { User } = require('../models');
 const { userNameValidation,
-  emailRequireValidation,
-  emailFormatValidation,
-  existEmailValidation,
-  passwordLengthValidation,
-  passwordRequireValidation } = require('../services/userValidation');
+  emailValidation,
+  passwordValidation,
+  } = require('../services/userValidation');
 
 const CREATED = 201;
 const INTERNAL_SERVER_ERROR = 500;
@@ -22,13 +20,12 @@ const Router = express.Router();
 
 Router.post('/',
   userNameValidation,
-  emailRequireValidation,
-  emailFormatValidation,
-  existEmailValidation, passwordLengthValidation, passwordRequireValidation, async (req, res) => {
+  emailValidation,
+  passwordValidation, async (req, res) => {
   try {
-    const { userInfo } = await User.create(req.body);
-    delete userInfo.password;
-    const token = jwt.sign({ data: userInfo }, secret, jwtConfig);
+    const { dataValues } = await User.create(req.body);
+    delete dataValues.password;
+    const token = jwt.sign({ data: dataValues }, secret, jwtConfig);
     return res.status(CREATED).json({ token });
   } catch (error) {
     res.status(INTERNAL_SERVER_ERROR).json({ message: 'Something went wrong' });
--- services/userValidation.js
+++ services/userValidation.js
@@ -7,6 +7,11 @@ const CONFLICT = 409;
 // Verifica se nome de usuário tem pelo menos 8 caracteres
 const userNameValidation = (req, res, next) => {
   const { displayName } = req.body;
+  if (!displayName) {
+    return res.status(BAD_REQUEST).json({
+      message: '"displayName" length must be at least 8 characters long',
+    });
+  }
   if (displayName.length < 8) {
     return res.status(BAD_REQUEST).json({
       message: '"displayName" length must be at least 8 characters long',
@@ -16,28 +21,16 @@ const userNameValidation = (req, res, next) => {
 };
 
 // req 1
-// verifica se email é no formato correto e se já existe algum email registrado
-const emailRequireValidation = async (req, res, next) => {
+// verifica se email foi preenchido, se é no formato correto e se já existe algum email registrado
+const emailValidation = async (req, res, next) => {
   const { email } = req.body;
   if (!email) return res.status(BAD_REQUEST).json({ message: '"email" is required' });
-  next();
-};
-
-// req 1
-// Verifica se o formato de email é válido
-const emailFormatValidation = async (req, res, next) => {
-  const { email } = req.body;
+  
   const regexEmail = /[a-zA-Z]+@[a-zA-Z]+/;
   if (!regexEmail.test(email)) {
     return res.status(BAD_REQUEST).json({ message: '"email" must be a valid email' });
-  } 
-  next();
-};
+  }
 
-// req 1
-// Verifica se já existe usuário registrado
-const existEmailValidation = async (req, res, next) => {
-  const { email } = req.body;
   const existsEmail = await User.findOne({ where: { email } });
   if (existsEmail) return res.status(CONFLICT).json({ message: 'User already registered' });
   next();
@@ -45,27 +38,18 @@ const existEmailValidation = async (req, res, next) => {
 
 // req 1
 // verifica se senha foi preechida e se contém pelo menos 6 caracteres
-const passwordLengthValidation = (req, res, next) => {
+const passwordValidation = (req, res, next) => {
   const { password } = req.body;
+  if (!password) return res.status(BAD_REQUEST).json({ message: '"password" is required' });
+
   if (password.length < 6) {
     return res.status(BAD_REQUEST).json({ message: '"password" length must be 6 characters long' });
   }
   next();
 };
 
-// req 1
-// Verifica se o campo senha foi preenchido
-const passwordRequireValidation = (req, res, next) => {
-  const { password } = req.body;
-  if (!password) return res.status(BAD_REQUEST).json({ message: '"password" is required' });
-  next();
-};
-
 module.exports = {
   userNameValidation,
-  emailRequireValidation,
-  emailFormatValidation,
-  existEmailValidation,
-  passwordLengthValidation,
-  passwordRequireValidation,
+  emailValidation,
+  passwordValidation,
 };
