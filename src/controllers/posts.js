4ae9162b95db82fe7fc94711144384f2746a1917
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-10-19 18:17:03 -0300

    remove password posts controller

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -23,7 +23,7 @@ const addBlogPost = rescue(async (req, res, next) => {
 const getAllBlogPost = rescue(async (req, res) => {
   const allBlogPost = await BlogPost.findAll({ 
     include: [
-      { model: User, as: 'user' },
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
       { model: Category, as: 'categories', through: { attributes: [] } },
     ], 
   });
