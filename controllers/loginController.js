dddd3f9b28b3aeda62bb6108c74ea6548e6c6473
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-19 14:29:20 -0300

    Done: test of GitHub

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,16 +8,13 @@ const { JWT_SECRET } = process.env;
 const login = async (req, res) => {
   try {
     const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };
-
     const response = await loginUser(req.body);
-    // console.log(response, 'poioioioi');
     
     if (response.isError) {
       return res.status(StatusCodes.BAD_REQUEST).json({ message: response.message }); 
     }
     
     const token = jwt.sign({ data: response }, JWT_SECRET, jwtConfig);
-    
     return res.status(StatusCodes.OK).json({ token });
   } catch (error) {
     return res.status(500).json({ message: error.message });
