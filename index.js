3347c26bb9813984dd126d18c72eb1e11343b606
antonioarieiro <sfgfffs@gmail.com>
2021-10-19 23:14:24 -0300

    express and bodypareser

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require('dotenv/config');
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('rodandno fino na porta 3000!'));
+app.use(express.json());
+
+app.listen(3000, () => console.log(`rodandno fino na porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
-  response.send();
+  response.send('bagui é doido, la vamos nos dnv #vqv');
 });
