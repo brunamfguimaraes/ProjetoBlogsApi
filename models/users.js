fbd7c3564fb9ad71b5562010fcd9e5f89fb9a5e1
Matheus H. Gois <matheus.hengo@gmail.com>
2021-10-20 14:05:37 -0300

    User Model Created

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const User = (sequelize, DataTypes) => {
+  const UserIn = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  { timestamps: false, tableName: 'Users' });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { 
+      foreignKey: 'userId', as: 'blogPost',
+    });
+  };
+
+  return UserIn;
+};
+
+module.exports = User;
