0961b303e81c54cf7a0cf0a9c463f384b334dfa8
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-10-20 17:45:31 -0300

    requisito 08 fix lint

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { User } = require('./src/models');
 const route = require('./src/routes');
 
 const error = require('./src/middlewares/error');
 
 const app = express();
 app.use(bodyParser.json());
-
-app.get('/teste', async (req, res) => {
-  const result = await User.create({ displayName: 'teste', email: 'teste@teste.com', password: '123' });
-  res.json(result);
-});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
