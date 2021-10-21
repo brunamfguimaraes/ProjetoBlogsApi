d63cf17a218cbe04892fb48b91b9fbd786e35ba1
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-21 18:00:32 -0300

    Bug: put layers on endpoint to see if it causes an error

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const { createUser, getAllUsers /* findUser */ } = require('../controllers/usersController');
+const { createUser, getAllUsers, findUser } = require('../controllers/usersController');
 const validCreateUser = require('../middleware/validCreateUser');
 const validateJWT = require('../auth/validateJWT');
 
 const router = express.Router();
 
-router.get('/:id', validateJWT);
+router.get('/:id', validateJWT, findUser);
 
 router.get('/', validateJWT, getAllUsers);
 
