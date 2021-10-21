e171c7d2563a921abd34ed5bdea5fcef4bddcc2e
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-20 21:31:08 -0300

    Bug: change made to the location of app.listen

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ app.use('/login', loginRoutes);
 app.use('/user', usersRoutes);
 app.use('/categories', categoriesRoutes);
 //
+app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}`));
 //
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
-
-app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}`));
