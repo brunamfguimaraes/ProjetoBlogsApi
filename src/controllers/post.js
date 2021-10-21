f7ca683b33a92af31d1ceb1fb750969578ffc901
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-10-21 19:39:57 +0200

    Requirement 7

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,8 +1,8 @@
 const service = require('../services/post');
 
 async function createPost(req, res) {
-  const { title, content } = req.body;
-  const result = await service.createPost(title, content, req.user.id);
+  const { title, content, categoryIds } = req.body;
+  const result = await service.createPost(title, content, req.user.id, categoryIds);
   return res.status(201).json(result);
 }
 
--- src/services/post.js
+++ src/services/post.js
@@ -1,6 +1,15 @@
-const { BlogPosts } = require('../../models');
+const { in: OpIn } = require('sequelize').Op;
+const { BlogPosts, Category } = require('../../models');
+const { postValidation } = require('../validations/post');
 
-async function createPost(title, content, userId) {
+async function createPost(title, content, userId, categoryIds) {
+  postValidation(title, content, categoryIds);
+  const category = await Category.findAll({ where: { id: { [OpIn]: categoryIds } } });
+  if (!category.length) {
+    const error = new Error('"categoryIds" not found');
+    error.code = 400;
+    throw error;
+  }
   const result = await BlogPosts.create({ title, content, userId });
   return result;
 }
