5c7e4aca245dd5bf2c2e11c964d32b45ee8eb403
Breno Rodrigues <brenorm@gmail.com>
2021-10-21 22:48:43 -0300

    Requisito 4: Criação de rota, aplicação de middleware de validação de token, validação de usuário inexistente

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,5 +47,24 @@ userRouter.get('/', validateJWT, async (req, res) => {
 });
 
 // ---------------------------------------------------------------
+// Requisito 4: CONTROLLER responsável por realizar busca de usuário por ID via sequelize e retornar o usuário cadastrado.
+
+userRouter.get('/:id', validateJWT, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({ where: { id } });
+
+    if (!user) {
+      res.status(404).json({ message: 'User does not exist' });
+    }
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+// ---------------------------------------------------------------
 
 module.exports = { userRouter };
