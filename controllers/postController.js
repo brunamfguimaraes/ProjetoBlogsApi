0031fac075dd23a674faebcbbe5263e6104f7698
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-10-23 02:15:16 -0300

    Bug: testing

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,18 +3,16 @@ const { createPostServices } = require('../services/postServices');
 
 const createPost = async (req, res) => {
   const { title, content, categoryIds } = req.body;
-  console.log(req.user);
   const { id } = req.user;
   try {
     const response = await createPostServices({ title, content, categoryIds, id });
-    // console.log(response);
     if (response.isError) {
       return res.status(StatusCodes.BAD_REQUEST).json({ message: response.message });
     } 
     return res.status(StatusCodes.CREATED).json(response);
   } catch (error) {
     // console.log('PASSEI NO CATCH');
-    console.log(error);
+    // console.log(error);
 
     return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
   }
--- services/postServices.js
+++ services/postServices.js
@@ -18,15 +18,17 @@ const createPostServices = async ({ title, content, categoryIds, id: userId }) =
 
   const { id: postId } = await BlogPost.create({ title, content, userId });
 
-  await categoryIds.forEach(async (categoryId) => {
-    await PostCategory.create({ postId, categoryId });
-  });
+  // console.log(PostCategory);
+  // const a = await categoryIds.map(async (categoryId) => {
+  //   await PostCategory.create({ postId, categoryId });
+  // });
+
+  // const B = await Promise.all(a); 
+
+  // const isNullb = B.some((e) => !e);
+  // console.log(isNullb, 'OIOIOIOIOIOIOI');
   
-  return { id: postId,
-    userId,
-    title,
-    content,
-  };
+  return { id: postId, userId, title, content };
 };
 
 module.exports = { createPostServices };
