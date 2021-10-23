72747d6ba613662dd897abdf124593bb952225b5
esmaben <felippecorrea2010@gmail.com>
2021-10-23 12:19:58 -0300

    Dev req07: funcao createPost criada no arquivo postService

---
+++ services/postService.js
@@ -0,0 +1,44 @@
+const { BlogPost, Categorie } = require('../models');
+
+const validateField = (title, content, categoryIds) => {
+    if (!title) {
+        return { message: '"title" is required' };
+    }
+
+    if (!content) {
+        return { message: '"content" is required' };
+    }
+   
+    if (!categoryIds) {
+        return { message: '"categoryIds" is required' };
+    }
+   
+    return true;
+};
+
+const notFoundCategory = async (categoryIds) => {
+    const categories = await Categorie.findAll({ where: { id: categoryIds } });
+    if (categories.length !== categoryIds.length) {
+        return { message: '"categoryIds" not found' };
+    }
+
+    return true;
+};
+
+const createPost = async (title, content, userId, categoryIds) => {
+    const fields = validateField(title, content, categoryIds);
+    if (fields !== true) return { message: fields.message };
+    
+    const notCategory = notFoundCategory(categoryIds);
+    if (notCategory !== true) return { message: notCategory.message };
+
+    const post = await BlogPost.create({
+        title, content, userId,
+    });
+
+    return post;    
+};
+
+module.exports = {
+    createPost,
+};
