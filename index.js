3c45b446a895a1f3fcbb6a293fc8d5e0938c36ba
Abimael <abimaelr.albu@gmail.com>
2021-10-23 15:52:58 -0300

    Rotas ligadas ao index

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require('express');
 const app = express();
 
 const user = require('./routes/user');
+const login = require('./routes/login');
+const post = require('./routes/post');
+const categories = require('./routes/categories');
 
 app.use(express.json());
-app.use('/user', user)
+
+app.use('user', user);
+app.use('login', login);
+app.use('post', post);
+app.use('categories', categories)
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
