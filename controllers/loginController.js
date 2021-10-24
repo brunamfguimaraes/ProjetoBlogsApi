021f389e71111210dbc0d7d5d139b505c2956410
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-10-24 13:08:24 -0300

    Criado o controller loginController

---
+++ controllers/loginController.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const { validateLogin } = require('../services/loginService');
+require('dotenv').config();
+
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const login = await validateLogin(email, password);
+  const { message } = login;
+  if (message) {
+    return res.status(400).json({ message });
+  }
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+};
+const token = jwt.sign({ data: login }, process.env.JWT_SECRET, jwtConfig);
+return res.status(200).json({ token });
+};
+
+module.exports = {
+  userLogin,
+};
