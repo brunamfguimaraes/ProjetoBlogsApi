121b117bf80415d2b5ae3b97af22fbdeb74bf26f
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-10-24 14:14:39 -0300

    requisito 3 - incompleto

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -2,13 +2,13 @@ const { verifyJWT } = require('./jwtFunctions');
 
 const authValidation = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization) return res.status(401).json({ message: 'missing auth token' });
+  if (!authorization) return res.status(401).json({ message: 'Token not foud' });
   try {
     const payload = verifyJWT(authorization);
     req.payload = payload;
     return next();
   } catch (error) {
-    res.status(401).json({ message: 'jwt malformed' });
+    res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const userService = require('../services/userService');
 const auth = require('../auth/jwtFunctions');
+const { authValidation } = require('../auth/authMiddleware');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  const { displayName, email, password, image } = req.body;
+  // const { displayName, email, password, image } = req.body;
   console.log(req.body);
-  console.log(displayName, email, password, image);
+  // console.log(displayName, email, password, image);
 
   try {
     const newUser = await userService.createUser(req.body);
@@ -21,6 +22,15 @@ router.post('/', async (req, res) => {
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+router.get('/', authValidation, async (_req, res) => {
+  try {
+    const users = await userService.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
 });
 
