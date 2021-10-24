90709e5f937a474a0b8ae3ccc2f8d474bf34d24d
Breno Rodrigues <brenorm@gmail.com>
2021-10-24 02:18:30 -0300

    Inserção de RETURNS faltantes

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ userRouter.get('/:id', validateJWT, async (req, res) => {
     const user = await User.findOne({ where: { id } });
 
     if (!user) {
-      res.status(404).json({ message: 'User does not exist' });
+      return res.status(404).json({ message: 'User does not exist' });
     }
 
     return res.status(200).json(user);
@@ -61,7 +61,7 @@ userRouter.get('/', validateJWT, async (req, res) => {
     return res.status(200).json(users);
   } catch (e) {
     console.log(e.message);
-    res.status(500).json({ message: 'Algo deu errado' });
+    return res.status(500).json({ message: 'Algo deu errado' });
   }
 });
 
