0fcd2def8ac5d3a8db96dd55686202aaf02304ac
Abimael <abimaelr.albu@gmail.com>
2021-10-24 09:02:28 -0300

    PORT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
@@ -14,7 +15,9 @@ app.use('login', login);
 app.use('post', post);
 app.use('categories', categories);
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
