c3e9f2fd1b418fb03d8d87a50064e03098423970
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-10-25 00:20:12 -0300

    :construction:Instalaçao dos pacotes e formataçao basica do index.js :construction:

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((error, _req, res, _next) => {
+  console.log(error);
+  res.status(error.status).json({ message: error.message });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
