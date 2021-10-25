d93de56fe6a5cb312c546069bb9f5c1f1d89bb6e
Abimael <abimaelr.albu@gmail.com>
2021-10-25 10:14:27 -0300

    returns

--- middlewares/post.js
+++ middlewares/post.js
@@ -2,9 +2,9 @@ const { Categories } = require('../models');
 
 const haveFields = (req, res, next) => {
     const { title, content, categoryIds } = req.body;
-    if (!title) res.status(400).json({ message: '"title" is required' });
-    else if (!content) res.status(400).json({ message: '"content" is required' });
-    else if (!categoryIds) res.status(400).json({ message: '"categoryIds" is required' });
+    if (!title) return res.status(400).json({ message: '"title" is required' });
+    if (!content) return res.status(400).json({ message: '"content" is required' });
+    if (!categoryIds) return res.status(400).json({ message: '"categoryIds" is required' });
     next();
 };
 
@@ -14,9 +14,9 @@ const haveCategory = async (req, res, next) => {
     .map((category) => Categories.findOne({ where: { id: category } }));
     const result = await Promise.all(have);
     if (result.includes(null)) {
- return res
-        .status(400).json({ message: '"categoryIds" not found' }); 
-}
+        return res
+            .status(400).json({ message: '"categoryIds" not found' }); 
+    }
     next();
 };
 
