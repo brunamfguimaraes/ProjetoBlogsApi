d75ba4be1fa4f09e3acecb1cbd0ec8f92cfb1e27
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-10-25 19:55:11 -0300

    Resolvido falha no evaluator do github

--- middleware/validateJWT.js
+++ middleware/validateJWT.js
@@ -1,29 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-// const { User } = require('../models');
-
-// const secret = 'seusecretdetoken';
-
-// const validateJWT = async (req, res, next) => {
-//   const token = req.headers.authorization;
-//   if (!token) { return res.status(401).json({ message: 'Token not found' }); }
-//   try {
-//     const decoded = jwt.verify(token, secret);
-//     const user = await User.findOne(
-//         { email: decoded.data.email },
-//     );
-//     if (!user) {
-//         return res.status(401).json({ message: 'Expired or invalid token' }); 
-//     }
-//     req.user = user;
-//     next();
-//   } catch (_err) {
-//     return res.status(401).json({ message: 'Expired or invalid token' });
-//   }
-// };
-
-// module.exports = validateJWT;
-
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 require('dotenv').config();
