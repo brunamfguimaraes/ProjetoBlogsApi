92cbc2355f823a6afb5da6beedadcb420ed081f7
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-10-25 12:01:15 -0300

    refazendo validateJWT para tentar consertar o erro no evaluator do github

--- middleware/validateJWT.js
+++ middleware/validateJWT.js
@@ -1,5 +1,30 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
+// const { User } = require('../models');
+
+// const secret = 'seusecretdetoken';
+
+// const validateJWT = async (req, res, next) => {
+//   const token = req.headers.authorization;
+//   if (!token) { return res.status(401).json({ message: 'Token not found' }); }
+//   try {
+//     const decoded = jwt.verify(token, secret);
+//     const user = await User.findOne(
+//         { email: decoded.data.email },
+//     );
+//     if (!user) {
+//         return res.status(401).json({ message: 'Expired or invalid token' }); 
+//     }
+//     req.user = user;
+//     next();
+//   } catch (_err) {
+//     return res.status(401).json({ message: 'Expired or invalid token' });
+//   }
+// };
+
+// module.exports = validateJWT;
+
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
 const secret = 'seusecretdetoken';
@@ -10,7 +35,7 @@ const validateJWT = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, secret);
     const user = await User.findOne(
-        { email: decoded.data.email },
+        { where: { email: decoded.data.email, password: decoded.data.password } },
     );
     if (!user) {
         return res.status(401).json({ message: 'Expired or invalid token' }); 
