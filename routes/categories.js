c3993dd0745f41485c4e02994cad5a5ffb90222a
Abimael <abimaelr.albu@gmail.com>
2021-10-24 22:53:56 -0300

    req 5

--- routes/categories.js
+++ routes/categories.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-// const middleware = require('../middlewares/categories');
+const middleware = require('../middlewares/categories');
+const token = require('../middlewares/token');
+const controller = require('../controllers/categories');
 
 router.route('/')
     .get()
-    .post();
+    .post(
+        token.haveToken,
+        token.validToken,
+        middleware.haveName,
+        controller.create,
+    );
 
 module.exports = router;
