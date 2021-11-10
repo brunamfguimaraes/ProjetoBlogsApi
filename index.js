5b49785f417f8e9556169cb7209ce2083d541d0c
marcos <marcosd3wow@gmail.com>
2021-11-10 15:29:41 -0300

    configuraçoes iniciais no arquivo index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+require('dotenv').config();
+// app.listen(3000, () => console.log('ouvindo porta 3000!'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
