707a8959ee5f0d38c0a1bbd83c2c5baf706fc7a9
Gildeir <gildeirlopes@gmail.com>
2021-11-11 22:41:30 -0300

    Adicionando funçao getUserById

--- controllers/userController.js
+++ controllers/userController.js
@@ -146,6 +146,24 @@ const login = async (req, res) => {
 }
 };
 
+// Este endpoint usa o método findByPk do Sequelize para buscar um usuário pelo id.
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id, {
+      attributes: { exclude: ['password'] },
+    });
+
+    if (!user) {
+    return res.status(404).json({ message: 'User does not exist' }); 
+}
+
+return res.status(200).json(user);
+} catch (error) {
+  return res.status(400).json({ message: error.message });
+}
+};
+
 module.exports = {
   validateEmail,
   checkPassword,
@@ -160,4 +178,5 @@ module.exports = {
   jwtTokenFunc,
   loginFunction,
   login,
+  getUserById,
 };
