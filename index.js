e744efb8fc78424d2072244e07b429e647c04493
marcos <marcosd3wow@gmail.com>
2021-11-13 12:02:25 -0300

    configurando index.js, criando post /user

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 
+const User = require('./api/controllers/User');
+const errorMiddleware = require('./api/middlewares/errorMiddleware');
+
 const app = express();
 app.use(express.json());
 
@@ -10,8 +13,12 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
+app.post('/user', User.registerNewUser);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(errorMiddleware);
 // iniciando projeto
