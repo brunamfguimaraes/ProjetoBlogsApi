586850fccd8a08745f73e10f1b2a007534e33538
Gildeir <gildeirlopes@gmail.com>
2021-11-13 04:00:54 -0300

    refatorando requisitos 8 e 9 getPost/:id

--- controllers/editPostController.js
+++ controllers/editPostController.js
@@ -1,10 +1,13 @@
 const { User, BlogPost, Category } = require('../models');
 
-  const editBlogPostFunction = async ({ id }, { title, content, categoryIds }, res) => {
-  if (!title) { return res.status(400).json({ message: '"title" is required' }); }
-  if (!content) return res.status(400).json({ message: '"content" is required' });
-  if (categoryIds) return res.status(400).json({ message: 'Categories cannot be edited' });
-  
+const validatePost = (title, content, categoryIds) => {
+    if (!title) { return { err: { status: 400, message: '"title" is required' } }; }
+    if (!content) return { err: { status: 400, message: '"content" is required' } };
+    if (categoryIds) return { err: { status: 400, message: 'Categories cannot be edited' } };
+    return true;
+};
+
+  const editBlogPostFunction = async ({ id }, { title, content }) => {
   await BlogPost.update({ ...BlogPost, title, content }, { where: { id } });
   const updatedPost = await BlogPost.findByPk(id, 
   { 
@@ -14,25 +17,23 @@ const { User, BlogPost, Category } = require('../models');
 };
 
 const editBlogPost = async (req, res) => {
-  try {
     const { id } = req.params;
     const email = req.user;
-    const { id: userId } = await User.findOne({ where: { email } }); 
-    console.log('userId: ', userId);
     const { title, content, categoryIds } = req.body;
+    const isValidPost = validatePost(title, content, categoryIds);
+    if (validatePost !== true) {
+      return res.status(isValidPost.err.status).json({ message: isValidPost.err.message }); 
+    }
+    
+    const { id: userId } = await User.findOne({ where: { email } });     
     const postId = await BlogPost.findByPk(id);
-    console.log('postId:', postId);
-   const blogPost = await editBlogPostFunction({ id }, { title, content, categoryIds }, res);
-  if (postId.id !== userId) {
+    const blogPost = await editBlogPostFunction({ id }, { title, content, categoryIds });
+    
+    if (postId.id !== userId) {
         return res.status(401).json({ message: 'Unauthorized user' });
      }
 
     return res.status(200).json(blogPost);
-  } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
-  }
 };
 
 module.exports = {
