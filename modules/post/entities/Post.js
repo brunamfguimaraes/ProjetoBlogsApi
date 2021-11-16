a9756f5a943f55c0278987e85afaa3724a5ca2a6
brunobastosfer <brunobastos@edu.unifor.br>
2021-11-16 16:35:15 -0300

    criando as entidades e conexão com o BD

---
+++ modules/post/entities/Post.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {}, { timestamps: false,
+    tableName: 'PostsCategories',
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postID',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postID',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
