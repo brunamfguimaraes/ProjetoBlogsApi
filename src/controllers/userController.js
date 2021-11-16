7561068dab1f023167ac2d31a778806d3cfaea6e
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-16 20:51:09 -0300

    Refatora funções de criação de usuário

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const Sequelize = require('sequelize');
-const { User } = require('../database/models');
-const config = require('../database/config/config');
 
-const sequelize = new Sequelize(config.development);
+const { createNewUser } = require('../service/userService');
+
 const UserRouter = express.Router();
 
 UserRouter.post('/', async (req, res) => {
   try {
     const { displayName, email, password, image } = req.body;
 
-    const result = await sequelize.transaction(async (t) => {
-      const user = await User.create({ displayName, email, password, image }, { transaction: t });
+    const { errorMessage } = await createNewUser(displayName, email, password, image);
 
-      return user;
-    });
+    if (errorMessage) {
+      return res.status(400).send(errorMessage);
+    }
     
-    return res.status(201).send({ message: 'Usuário criado com sucesso', user: result });
+    return res.status(201).send({ message: 'Usuário criado com sucesso' });
   } catch (e) {
     res.status(500).send({ message: e.message });
   }
--- src/service/userService.js
+++ src/service/userService.js
@@ -4,26 +4,66 @@ const config = require('../database/config/config');
 
 const sequelize = new Sequelize(config.development);
 
-const validName = (name) => name.lenght >= 8;
+const validName = (name) => {
+  if (name.lenght < 8) {
+    return {
+      message: 'name must have at least 8 characters',
+    };
+  }
+};
 
 const validEmail = (email) => {
   // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!email || !re.test(email)) return false;
+  if (!email) return { message: 'email must be filled' };
+  if (!re.test(email)) return { message: 'invalid email format' };
 };
 
 const emailAlreadyInUse = async (email) => {
   const user = await User.findOne({ where: { email } });
 
-  console.log(user);
+  if (user) return { message: 'email already in use' };
 };
 
-const validPassword = (password) => password.lenght >= 6;
+const validPassword = (password) => {
+  if (password.toString().length < 6) {
+    return { 
+      message: 'Password must have at least 6 characters', 
+    }; 
+  }
+};
+
+const isUserFieldsInvalid = async (displayName, email, password) => {
+  const dataCheck = await Promise.all([
+    emailAlreadyInUse(email),
+    validName(displayName),
+    validEmail(email),
+    validPassword(password),
+  ]);
+ 
+  const invalidFieldsMessage = dataCheck.filter((e) => e !== undefined);
+
+  return invalidFieldsMessage[0];
+};
 
 const createNewUser = async (displayName, email, password, image) => {
+  const invalidUserMessage = await isUserFieldsInvalid(displayName, email, password);
+
+  if (invalidUserMessage) return { errorMessage: invalidUserMessage };
+
   const result = await sequelize.transaction(async (t) => {
     const user = await User.create({ displayName, email, password, image }, { transaction: t });
 
     return user;
   });
+
+  return result;
+};
+
+module.exports = {
+  validName,
+  validEmail,
+  validPassword,
+  emailAlreadyInUse,
+  createNewUser,
 };
