c4020e7bf90dc0a8ce072472513a71990b9457d7
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-16 21:30:01 -0300

    Adiciona função que gera o token

---
+++ src/service/jwtService.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
+
+const jwtConfig = {
+  expiresIn: '1h',
+  algorithm: 'HS256',
+};
+
+const generateJwtToken = async (user) => {
+  try {
+    const payLoad = { data: user };
+
+    const token = jwt.sign(payLoad, secret, jwtConfig);
+  
+    return token;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+module.exports = {
+  generateJwtToken,
+};
