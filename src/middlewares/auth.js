4706314d72d722f7e3f753a610f324d15923c198
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-16 23:25:34 -0300

    Corrige o auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,25 +1,14 @@
 const jwt = require('jsonwebtoken');
-const { findUserByEmail } = require('../service/userService');
 
 module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ error: 'Token não encontrado' });
+    return res.status(401).json({ error: 'Token not found' });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.SECRET);
-
-    const user = await findUserByEmail.findUser(decoded.data.userEmail);
-
-    if (!user) {
-    return res.status(401).json({
-      message: 'Não foi possível encontrar o email do usuário informado no token',
-    }); 
-}
-
-    req.userData = { user, admin: decoded.admin };
+    await jwt.verify(token, process.env.JWT_SECRET);
 
     next();
   } catch (err) {
