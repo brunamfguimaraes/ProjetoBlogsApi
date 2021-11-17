af8ee6049cd47dac9611dfe608c49f4cd054639b
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-11-16 23:13:23 -0300

    Muda os nomes das tabelas para primeira letra maiuscula

--- src/database/migrations/20211116175927-create-blog-posts.js
+++ src/database/migrations/20211116175927-create-blog-posts.js
@@ -30,7 +30,7 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
         references: {
-          model: 'users',
+          model: 'Users',
           key: 'id',
         }
       },
