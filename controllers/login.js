996255093dd654381c002dfe9282f7b24785f2d3
Ricardo <ricardo.rals.dev@gmail.com>
2021-11-18 11:33:35 -0300

    fix req 2

--- controllers/login.js
+++ controllers/login.js
@@ -10,12 +10,16 @@ const jwtConfig = {
 };
 
 const login = async (req, res) => {
-    const { email } = req.body;
-    const userLogged = await User.findOne({ where: { email } });
-    if (!userLogged) res.status(400).json({ message: 'Invalid fields' });
-    
-    const token = jwt.sign({ email }, SECRET, jwtConfig);
-    res.status(200).json({ token }); 
+    try {
+      const { email } = req.body;
+      const userLogged = await User.findOne({ where: { email } });
+      if (!userLogged) return res.status(400).json({ message: 'Invalid fields' });
+
+      const token = jwt.sign({ userLogged }, SECRET, jwtConfig);
+      res.status(200).json({ token });
+    } catch (e) {
+      return res.status(500).json({ message: 'Erro ao logar' });
+    }
 };
 
 module.exports = {
